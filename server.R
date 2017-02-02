4f6a19aca568b47da49eeb4fdf03d61da11ccd8e
Darren Local <darrenc@wheatyield.anu.edu.au>
2017-02-02 13:02:46 +1100

    version 0.1 working edition. Waiting feedback on what parts need to be tweaked. Also, help page only contains an e-mail address...

--- server.R
+++ server.R
@@ -2,39 +2,42 @@ library("shiny")
 library("reshape2")
 library("plotly")
 library("ggplot2")
+library("plyr")
 library("XLConnectJars")
 library("XLConnect")
 
-function(input,output){
+
+
+function(input,output,session){
 
   
-  output$data_notice <- renderText("Choose a valid xls(x) data file")
-  output$meta_notice <- renderText("Choose a valid xls(x) data file")
-  output$fluoro_notice <- renderText("Please ensure a valid Q2 data is loaded")
-  output$slopes_notice <- renderText("Please ensure a valid Q2 data is loaded")
+  output$data_notice <- renderText("Choose a valid .xls(x) data file")
+  output$meta_notice <- renderText("Download, complete and upload a metadata file")
   output$respiration_notice <- renderText("Please ensure valid Q2 data, Q2 metadata and experimental conditions are loaded")
   
   
-  # data <- reactive({rnorm(input$num)})
-  # 
-  # output$hist <- renderPlot({
-  #   title <- paste(as.character(input$num),"Random Numbers")
-  #   hist(data(), main = title)
-  # })
-  # 
-  # output$stats <- renderPrint(summary(data()))
+  
+  well_cols <- c("A","B","C","D","E","F")
+
+  wells <- NULL
+  for(i in 1:8){
+    for(j in well_cols){
+      wells <- c(wells,paste0(j,i))
+    }
+  }   
+
+  
 
   Q2_data <- reactive({
   
     if (!is.null(input$xlsInput)){
-      
-      #NB: MUST put in here a way to catch an exception of trying to put in a non-XLS(x) file!
+
       plate_data <- readWorksheetFromFile(input$xlsInput$datapath,sheet=1,header=FALSE)
       
       if (plate_data[1,1] != "Q2 RUN informaton"){
         
         output$data_notice <- renderText("Does not appear to be a Q2 data file")
-        plate_data <- NULL
+        # plate_data <- NULL
         return(plate_data)
         
       } else {
@@ -44,258 +47,307 @@ function(input,output){
         plate_data <- plate_data[-(1:21),]
         names(plate_data) <- plate_data[1,]
         plate_data <- plate_data[-1,]
+
              
         for(X in 0:(length(plate_data$HRS)-1)) {
           plate_data$HRS[[X+1]] <- formatC(X*interval, width=3, flag="0")
         }
         
+        plate_data$HRS <- as.integer(plate_data$HRS)
+        colnames(plate_data)[which(names(plate_data) == "HRS")] <- paste("MINS")
+        
         output$data_notice <- renderText("Q2 data file appears to be valid")
         return(plate_data)
       }
-    }  
-  })
-  
-  
-  
-  Q2_melted <- reactive({
-    
-    if (!is.null(Q2_data)){
-    
-      wells <- c("A","B","C","D","E","F")
-      
-      y_val <- NULL
-      for(i in 1:8){
-        for(j in wells){
-          y_val <- c(y_val,paste0(j,i))
-        }
-      }    
-      
-      melting <- melt(Q2_data(), measure.vars = y_val)
-      melting$value <- as.numeric(melting$value)
     }
-    return(melting)
+    
   })
   
   
+  
+  output$downloadTemplate <- downloadHandler(
+    filename = function() {
+      "Q2_metadata_template.xlsx"
+    },
+    content = function(file) {
+      myfile <- srcpath <- "/home/darrenc/R/templates/Q2_metadata_template.xlsx"
+      file.copy(myfile, file)
+    }
+  )
+
+  
       
-  Q2_metadata <- reactive({
+  Q2_plate_metadata <- reactive({
     
     if (!is.null(input$metadata)){
     
-      plate_metadata <- readWorksheetFromFile(input$metadata$datapath,sheet=1,header=FALSE)
-      if ((plate_metadata[2,1] != "Well") && (plate_metadata[1,1] != "Plate")){
+      plate_metadata <- readWorksheetFromFile(input$metadata$datapath,sheet=1,header=TRUE)
+      if (("Well" %in% colnames(plate_metadata)) && ("Plate" %in% colnames(plate_metadata))){
         
-        output$meta_notice <- renderText("Does not appear to be a valid metadata file")
+        output$meta_notice <- renderText("Q2 metadata file appears to be valid")
         return(plate_metadata)
           
       } else {
         
-        output$meta_notice <- renderText("Q2 metadata file appears to be valid")
+        output$meta_notice <- renderText("Does not appear to be a valid metadata file")
         return(plate_metadata)
         
       }
-      
-    }
-  })
-    
-    
-  output$fluoro <- renderPlotly(
-    
-    if (!is.null(input$xlsInput)){
-      
-      # Q2_plot <- plot_ly(x = ~Q2_data()$HRS, y = ~Q2_data()$A1, type = 'scatter', mode = 'lines', yaxis = list(range = c(-0.2, 1.2)), title = "Q2 Flourescence", evaluate = TRUE)
-      
-      fluoro_plot <- ggplot(Q2_melted(), aes(x=HRS, y=value, group=variable, color=variable)) +
-        geom_line() +
-        labs(x = "Time (min)", y = "Q2 Fluorescence", title = "Q2 Flourescence over Time") +
-        scale_y_continuous(limits = c(-0.2, 1.2), breaks = seq(-0.2,1.2,by=0.2), expand=c(0,0)) +
-        theme(axis.text.x = element_text(angle = 90, hjust = 1))
-      
-      if (!is.null(input$startTime) && !is.null(input$duration)){
-
-        fluoro_plot <- fluoro_plot + geom_vline(aes(xintercept = as.integer(input$startTime))) + geom_vline(aes(xintercept = as.integer(input$duration)))
-
-      }
-      else if (!is.null(input$startTime) && is.null(input$duration)){
-
-        fluoro_plot <- fluoro_plot + geom_vline(aes(xintercept = as.integer(input$startTime)))
-
-      }
-      else if (is.null(input$startTime) && !is.null(input$duration)){
-
-        fluoro_plot <- fluoro_plot + geom_vline(aes(xintercept = as.integer(input$duration)))
-
-      }
-    
-      # for(q in 1:8){
-      #   for(r in wells){
-      #     y_val <- paste0(r,q)
-      # 
-      #     add_trace(Q2_plot, x = ~HRS, y = ~y_val, type = 'scatter', mode = 'lines', yaxis = list(range = c(-0.2, 1.2)), title = "Q2 Flourescence", evaluate = TRUE)
-      #   }
-      # }
-      
-      # for(i in 1:8){
-      #   for(j in wells){
-      #     y_val <- paste0(j,i)
-      #     
-      #     calc_plot(Q2_plot,y_var)
-      #   }
-      # }
-      
-      # add_trace(Q2_plot, x = ~HRS, y = ~B1, type = 'scatter', mode = 'lines', yaxis = list(range = c(-0.2, 1.2)), title = "Q2 Flourescence")
-      
-      # Q2_plot
-    }
-    
-  )  
-
-
-  
-          
-  
-
-    
-  output$respiration <- reactive(
-    
-    if ((!is.null(input$xlsInput) && !is.null(input$metadata))){
-      return(NULL)
-      
-      
-    }
-    
-  )  
-  
-    
-  
-    # for(X in 1:length(plate_03)){
-    #   for(Y in 1:length(plate_03[[X]])){
-    #     print(plate_03[[X]][[Y]])
-    #     if(is.na(as.integer(plate_03[[X]][[Y]]))) {
-    #       next
-    #     }
-    #     else if(as.double(plate_03[[X]][[Y]]) == floor(as.double(plate_03[[X]][[Y]]))){
-    #       plate_03[[X]][[Y]] <- as.integer(plate_03[[X]][[Y]])
-    #     }
-    #     else {
-    #       plate_03[[X]][[Y]] <- as.double(plate_03[[X]][[Y]])
-    #     }
-    #   }
-    # }
-    # 
-    # 
-    # for(X in 0:(length(plate_03$data$HRS)-1)) {
-    #   plate_03$data$HRS[[X+1]] <- as.integer(X*plate_03$run$interval)
-    # }
-    
-    
-    # if (is.null(input$xlsInput)) {
-    #   
-    #   output$Notice <- renderText({print("Not a Q2 Data File")})
-    #   
-    # }  
-    # } else if (plate_data[1,1] == "Q2 RUN informaton") {
-    #   
-    #   return(plate_data)
-    #   
-    # } 
-    
-    
-  output$Q2_Data <- renderTable({
-    
-    if (is.null(Q2_data()) || colnames(Q2_data()[1]) != "ID"){
-      
-      Q2_data()
-
-    } else if (colnames(Q2_data()[1]) == "ID") {
-
-      Q2_data()
-      
     }
   })
   
   
-  output$Q2_Meta <- renderTable({
+  
+  Q2_Air_Nitro <- reactive({
     
-    if (is.null(Q2_metadata())){
+    airNitro <- NULL
+    
+    airNitro_temp <- as.data.frame(subset(Q2_data()[,7:10],Q2_data()$MINS >= as.numeric(input$UI_times[1]) & Q2_data()$MINS <= as.numeric(input$UI_times[2])), stringsAsFactors = FALSE)
+    
+    airNitro_temp[,1] <- as.numeric(unlist(airNitro_temp[,1]))
+    airNitro_temp[,2] <- as.numeric(unlist(airNitro_temp[,2]))
+    
+    airNitro_temp[,3] <- as.numeric(unlist(airNitro_temp[,3]))
+    airNitro_temp[,4] <- as.numeric(unlist(airNitro_temp[,4]))
+    
+    airNitro$nitroMean <- mean(c(airNitro_temp[,1],airNitro_temp[,2]))
+    airNitro$airMean <- mean(c(airNitro_temp[,3],airNitro_temp[,4]))
+    airNitro$ratio <- airNitro$airMean-airNitro$nitroMean
+
+    return(airNitro)
+    
+  })
+  
+
+  observeEvent({input$tabs == "Q2_fluoro_tab"},
+    
+    if (is.null(input$xlsInput) || (is.null(Q2_data())) || (colnames(Q2_data()[1]) != "ID")){
       
-      Q2_metadata()
+      output$fluoro_notice <- renderText("Please ensure a valid Q2 data file is loaded") 
+      
+    } else if (colnames(Q2_data()[1]) == "ID"){
+
+      output$fluoro_notice <- renderText("")
+      output$fluoro <- renderPlotly(
+        
+      if (!is.null(input$xlsInput)){
+        
+        melting <- melt(Q2_data(), measure.vars = wells)
+        melting$value <- as.numeric(melting$value)
+
+        fluoro_plot <- ggplot(melting, aes(x=MINS, y=value, group=variable, color=variable)) + geom_line()
+        fluoro_plot <- fluoro_plot + labs(x = "Time (min)", y = "Q2 Fluorescence", title = "Q2 Flourescence over Time")
+        fluoro_plot <- fluoro_plot + scale_y_continuous(limits = c(-0.2, 1.2), breaks = seq(-0.2,1.2,by=0.2), expand=c(0,0))
+        fluoro_plot <- fluoro_plot + scale_x_continuous(limits = c(min(Q2_data()$MINS, na.rm = TRUE),max(Q2_data()$MINS, na.rm = TRUE)), breaks = seq(min(Q2_data()$MINS, na.rm = TRUE),max(Q2_data()$MINS, na.rm = TRUE),by=100), expand=c(0,0))
+        fluoro_plot <- fluoro_plot + theme(axis.text.x = element_text(hjust = 1))
+        fluoro_plot <- fluoro_plot + theme(axis.title.x = element_text(vjust = -0.5))
+        
+      } 
+      )
+    
+      if (is.null(input$UI_times)){
+        output$fluoro_slider <- renderUI(sliderInput("UI_times","Start time and Duration",
+                                min = as.numeric(paste0(min(Q2_data()$MINS, na.rm = TRUE))),
+                                max = as.numeric(paste0(max(Q2_data()$MINS, na.rm = TRUE))),
+                                value = c(as.numeric(paste0(min(Q2_data()$MINS, na.rm = TRUE))),as.numeric(paste0(max(Q2_data()$MINS, na.rm = TRUE)))),
+                                step = as.numeric(Q2_data()$MINS[2]),
+                                dragRange = TRUE, post = " min")
+        )
+      }
+    }
+  )
+  
+  
+
+  Q2_slopes <- reactive({
+
+    Q2_subset <- subset(Q2_data(),Q2_data()$MINS >= as.numeric(input$UI_times[1]) & Q2_data()$MINS <= as.numeric(input$UI_times[2]))
+    
+    Q2_subset_temp <- Q2_subset[,7:54]
+    Q2_subset_temp <- Q2_subset_temp[,wells]
+    Q2_subset[,7:54] <- Q2_subset_temp
+    
+    slopes <- NULL
+    
+    for (Y in wells){
+
+      holder <- lm(Q2_subset[[paste(Y,sep="")]] ~ Q2_subset$MINS)
+      slopes[[paste(Y,sep="")]] <- print(holder$coefficients[[2]])
+
+      }
+    # slopes <- cbind(wells, slopes)
+    slopes <- cbind(wells, slopes)
+    
+    slopes[,2] <- as.numeric(slopes[,2])*60
+    
+    slopes[-(1:4),2] <- as.numeric(slopes[-(1:4),2])-mean(c(as.numeric(slopes[3,2]),as.numeric(slopes[4,2])))
+    
+    return(slopes)
+
+  })
+  
+
+  
+  Q2_Respiration <- reactive({
+    
+    temp_Q2_plate_metadata <- NULL
+    
+    meta <- GoodMeta()
+    
+    metafile <- input$metadata
+    altitude <- as.numeric(input$altitude)
+    sitePressure <- as.numeric(input$sitePressure)
+    tubeVol <- as.numeric(input$tubeVol)
+    temperature <- as.numeric(input$temperature)
+    
+    pressureCalc <- sitePressure-(101.35*(1-(1-(altitude/44307.69231))^5.25328))
+                                      
+    if (!is.null(input$xlsInput) && (!is.null(input$UI_times)) && (meta == "0") && (!is.null(Q2_Air_Nitro()$ratio))){
+      
+      airN2Ratio <- as.numeric(Q2_Air_Nitro()$ratio)
+      
+      temp_Q2_slopes <- Q2_slopes()[-(1:4),2]
+
+      temp_Q2_plate_metadata <- Q2_plate_metadata()[-(1:4),]
+      
+      temp_Q2_slopes <- lapply(temp_Q2_slopes, as.character)
+      temp_Q2_slopes <- as.numeric(temp_Q2_slopes)
+      
+      temp_Q2_plate_metadata$Area <- as.numeric(temp_Q2_plate_metadata$Area)
+      temp_Q2_plate_metadata$Fresh_Weight <- as.numeric(temp_Q2_plate_metadata$Fresh_Weight)
+      temp_Q2_plate_metadata$Dry_Weight <- as.numeric(temp_Q2_plate_metadata$Dry_Weight)
+
+      temp_Q2_plate_metadata$Area_Resp <- -(pressureCalc*((20.95/100)*tubeVol*temp_Q2_slopes/airN2Ratio)/(8314*(273.15+temperature))/(60*60)*1000000*(10000/temp_Q2_plate_metadata$Area))
+      temp_Q2_plate_metadata$Fresh_Resp <- -(pressureCalc*((20.95/100)*tubeVol*temp_Q2_slopes/airN2Ratio)/(8314*(273.15+temperature))/(60*60)*1000000*(1/temp_Q2_plate_metadata$Fresh_Weight)*1000)
+      temp_Q2_plate_metadata$Dry_Resp <- -(pressureCalc*((20.95/100)*tubeVol*temp_Q2_slopes/airN2Ratio)/(8314*(273.15+temperature))/(60*60)*1000000*(1/temp_Q2_plate_metadata$Dry_Weight)*1000)
+    }
+    
+    return(temp_Q2_plate_metadata)
+    
+  })
+  
+  
+  
+  output$respiration <- renderTable(digits = 4,Q2_Respiration()[,-1])
+
+  
+  
+  observe({
+    if ((!is.null(Q2_Respiration())) && (!is.null(input$UI_times))){  
+      output$Resp_Area <- renderPlotly({
+        resp_area_plot <- ggplot(Q2_Respiration(), aes(x=Well, y=Area_Resp)) + geom_bar(stat="identity")
+        resp_area_plot <- resp_area_plot + labs(x = "Well (Q2)",y = paste0("\u03BC","mol Oxygen /m*m/s"), title = "Respiration based on Leaf Area")
+        resp_area_plot <- resp_area_plot + scale_y_continuous(expand=c(0,0))
+        resp_area_plot <- resp_area_plot + theme(axis.title.x = element_text(vjust = -0.5),axis.title.y = element_text(hjust = -1),axis.text = element_text(size=5)) + theme_bw()
+        resp_area_plot
+      
+      })
+    }
+  })
+  
+  
+  
+  observe({
+    if ((!is.null(Q2_Respiration())) && (!is.null(input$UI_times))){
+      output$Resp_Fresh_Mass <- renderPlotly({
+        resp_area_plot <- ggplot(Q2_Respiration(), aes(x=Well, y=Fresh_Resp)) + geom_bar(stat="identity")
+        resp_area_plot <- resp_area_plot + labs(x = "Well (Q2)",y = paste0("nmol Oxygen /g/s"), title = "Respiration based on Leaf Mass (Fresh)")
+        resp_area_plot <- resp_area_plot + scale_y_continuous(expand=c(0,0))
+        resp_area_plot <- resp_area_plot + theme(axis.title.x = element_text(vjust = -0.5),axis.title.y = element_text(hjust = -1),axis.text = element_text(size=5)) + theme_bw()
+        resp_area_plot
+      })
+    }
+  })
+
+  
+  
+  observe({
+    if ((!is.null(Q2_Respiration())) && (!is.null(input$UI_times))){
+      output$Resp_Dry_Mass <- renderPlotly({
+        resp_area_plot <- ggplot(Q2_Respiration(), aes(x=Well, y=Dry_Resp)) + geom_bar(stat="identity")
+        resp_area_plot <- resp_area_plot + labs(x = "Well (Q2)",y = paste0("nmol Oxygen /g/s"), title = "Respiration based on Leaf Mass (Dry)")
+        resp_area_plot <- resp_area_plot + scale_y_continuous(expand=c(0,0))
+        resp_area_plot <- resp_area_plot + theme(axis.title.x = element_text(vjust = -0.5),axis.title.y = element_text(hjust = -1),axis.text = element_text(size=5)) + theme_bw()
+        resp_area_plot
+      })
+    }
+  })
+  
+  
+  
+  output$data_download <- renderUI(
+    if (!is.null(Q2_Respiration()) && (!is.null(input$UI_times))){
+      downloadButton("downloadBtn", "Download Respiration Data")
+    }
+  )
+
+  
+
+  output$downloadBtn <- downloadHandler(
+    filename = function() {"Respiration_data.csv"},
+    content = function(file) {write.csv(Q2_Respiration(), file, row.names=FALSE)}
+  )
+
+  
+  
+  GoodMeta <- reactive({
+     
+    stuff <- NULL
+    
+    metafile <- input$metadata
+    altitude <- input$altitude
+    pressure <- input$sitePressure
+    tubeVol <- input$tubeVol
+    temperature <- input$temperature
+    
+    if (is.null(metafile)){
+      stuff <- c(stuff,"metadata file")
+    }
+
+    if ((altitude == "") || (is.na(as.numeric(altitude)*1))){
+      stuff <- c(stuff,"altitude")
+    }
+    
+    if ((pressure == "") || (is.na(as.numeric(pressure)*1))){
+      stuff <- c(stuff,"pressure")
+    }
+    
+    if ((tubeVol == "") || (is.na(as.numeric(tubeVol)*1))){
+      stuff <- c(stuff,"tube volume")
+    }
+    
+    if ((temperature == "") || (is.na(as.numeric(temperature)*1))){
+      stuff <- c(stuff,"temperature")
+    }
+    
+    if (!is.null(stuff)){
+      
+      output$respiration_notice <- renderText(c(print("Please check the following metadata: "),paste(stuff,collapse=", ")))
+      return("1")
       
     } else {
       
-      Q2_metadata()
-      
+      output$respiration_notice <- renderText("")
+      return("0")
+        
     }
+    
   })
   
-  
-  # output$Q2_Meta <- renderTable({
-  #   
-  #   if (!is.null(Q2_data()) && colnames(Q2_data()[1]) == "ID"){
-  #     Q2_data()
-  #   }
-  # })
-    # output$data_notice <-  renderText(
-    #   if (is.null(Q2_data())){
-    #     print("Please ensure data is like good")
-    # }
-    # )
-
 
   
-  # observe(inXLSFile <- input$xlsInput,
-  # 
-  #   
-  #   if (inXLSFile){
-  # 
-  #     output$Notice <- renderText({print("Please ensure files are uploaded and fields are complete")})
-  # 
-  #   }
-  #   else{
-  # 
-  #     output$contents <- renderTable({
-  # 
-  #       readWorksheetFromFile(inXLSFile$datapath,sheet=1)
-  # 
-  #   })
-  # }
-  # 
-  # )
-  # Q2_plate <- list()
-  # 
-  # reactive({ input$upload
-  #   output$notice <- renderText({print("Button pushed")})
-  # })
-  #   # inMeta <- list()
-    # # input$file1 will be NULL initially. After the user selects
-    # # and uploads a file, it will be a data frame with 'name',
-    # # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # # column will contain the local filenames where the data can
-    # # be found.
-    # inXLSFile <- input$xlsInput
-    # inMetaFile <- input$metadata
-    # inMeta$altitude <- input$altitude
-    # inMeta$sitePressure <- input$sitePressure
-    # # inMeta$airportPressure <- input$airportPressure
-    # inMeta$tubeVol <- input$tubeVol
-    # inMeta$temperature <- input$temperature
-    # 
-    # if (is.null(inXLSFile)
-    #   || is.null(inMetaFile)
-    #   || is.null(inMeta$altitude)
-    #   || is.null(inMeta$sitePressure)
-    #   || is.null(inMeta$tubeVol)
-    #   || is.null(inMeta$temperature)
-    #   ){
-    #   output$Notice <- renderText({print("Please ensure files are uploaded and fields are complete")})
-    #   #return(NULL)
-    # }
-    # else{
-    #   # assign(paste0("Q2_plates$plate_",sprintf("%02d",length(Q2_plate)+1),"$data"), readWorksheetFromFile(inXLSFile$datapath,sheet=1))
-    #   # assign(paste0("Q2_plates$plate_",sprintf("%02d",length(Q2_plate)+1),"$meta"), readWorksheetFromFile(inMetaFile$datapath,sheet=1))
-    #   # assign(paste0("Q2_plates$plate_",sprintf("%02d",length(Q2_plate)+1),"$conds"), inMeta)
-    #   }
+  output$Q2_Data <- renderTable({
     
+    Q2_data()
+ 
+  })
   
 
+  
+  output$Q2_Meta <- renderTable(
+    
+    Q2_plate_metadata()
+      
+  )
+  
 }
--- ui.R
+++ ui.R
@@ -3,64 +3,86 @@ library("plotly")
 library("XLConnectJars")
 library("XLConnect")
 
-navbarPage("Q2 Summariser",
+
+fluidPage(
+  
+  tags$h1("Q2 Summariser"),
+  tags$br(),
+  uiOutput("data_download"),
+  tags$br(),
+    
+  tabsetPanel(id = "tabs",
+    tabPanel("Q2 Data", value = "Q2_data_tab",
+      fluidRow(
+        column(3,
+          tags$h1("Upload Q2 Data File"),
+          fileInput("xlsInput", "", accept = c(".xls",".xlsx")),
+          textOutput("data_notice")  
+        )
+  
+      ),
+      fluidRow(
+        tableOutput("Q2_Data")
+      )),
+    tabPanel("Q2 Metadata", value = "Q2_metadata_tab",
+      fluidRow(
+        column(3,
+          tags$h1("Metadata File"),
+          downloadButton("downloadTemplate", "Download Metadata Template (.xlsx)"),
+          fileInput("metadata","", accept = c(".xls",".xlsx")),
+          textOutput("meta_notice"),
+          tags$h1("Q2 Conditions"),
+          textInput("altitude","Altitude (m)"),
+          textInput("sitePressure","Site Pressure (kpa)"),
+          #textInput("airportPressure","Airport Pressure (kpa)"),
+          textInput("tubeVol","Tube Vol (mL)"),
+          textInput("temperature","Temperature (C)")
+        ),
+        column(6,
+          tableOutput("Q2_Meta")
+      )
+      )),
+      
+    navbarMenu("Analysis",
+      tabPanel("Fluorescence", value = "Q2_fluoro_tab",
+        
+        textOutput("fluoro_notice"),
+        plotlyOutput("fluoro"),
+        uiOutput("fluoro_slider")
+        
+               
+        ),
+     tabPanel("Respiration", value = "Q2_resp_tab",
+        
+      fluidPage(
+        textOutput("respiration_notice"),
+        # column(4,
+        #   textOutput("slopes_notice"),
+        #   tableOutput("slopes")
+        #               
+        # ),
+        # column(8,
+        #   tableOutput("respiration"),
+        #   textOutput("respiration_notice")
+        # )
+        column(1,
+          tableOutput("respiration")
+        ),
+        column(6,offset=5,
+               plotlyOutput("Resp_Area"),
+               tags$br(),
+               plotlyOutput("Resp_Fresh_Mass"),
+               tags$br(),
+               plotlyOutput("Resp_Dry_Mass")
+        )
+      )
+    )
+    ),
+  tabPanel("Help", value = "Q2_help_tab",
+          tags$h2("Help and instructions are coming soon. In the meantime, contact Darren Cullerne for help!"),
+          tags$a(href="mailto:darren.cullerne@anu.edu.au", "darren.cullerne@anu.edu.au")
           
-          tabPanel("Q2 Data",
-            fluidRow(
-              column(3,
-                tags$h1("Upload Q2 Data File"),
-                fileInput("xlsInput", "", accept = c(".xls",".xlsx")),
-                textOutput("data_notice")  
-              )
-
-            ),
-            fluidRow(
-              tableOutput("Q2_Data")
-            )),
-          tabPanel("Q2 Metadata",
-            fluidRow(
-              column(3,
-                tags$h1("Metadata File"),
-                fileInput("metadata","", accept = c(".xls",".xlsx")),
-                textOutput("meta_notice"),
-                tags$h1("Q2 Conditions"),
-                textInput("altitude","Altitude (m)"),
-                textInput("sitePressure","Site Pressure (kpa)"),
-                #textInput("airportPressure","Airport Pressure (kpa)"),
-                textInput("tubeVol","Tube Vol (mL)"),
-                textInput("temperature","Temperature (C)"),
-                textInput("startTime", "Start Time (mins)"),
-                textInput("duration", "Duration (mins)")
-              ),
-              column(6,
-                tableOutput("Q2_Meta")
-            )
-            )),
-            
-          navbarMenu("Analysis",
-            tabPanel("Fluorescence",
-              
-              textOutput("fluoro_notice"),
-              plotlyOutput("fluoro")
-                     
-              ),
-            tabPanel("Slopes",
-              
-              textOutput("slopes_notice"),
-              tableOutput("slopes")
-                     
-              ),
-            tabPanel("Respiration", 
-              
-              textOutput("respiration_notice"),
-              tableOutput("respiration")
-                            
-              )
-            ),
-          tabPanel("Help")
-
-
-  # *Output("...") functions
-  #plotOutput("hist")
-  #textOutput("text")
   )
+  
+  )
+)
